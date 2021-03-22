520ef436152b3a23b085776dfde1220053c4ce35
TriCodeIT <bayu165kusuma@gmail.com>
2021-03-22 14:48:05 +0700

    OOP -> Object & Classes

--- Object-Oriented/Object Everywhere/Contoh 1/src/App.kt
+++ Object-Oriented/Object Everywhere/Contoh 1/src/App.kt
@@ -1,7 +1,15 @@
 // main function
+//fun main() {
+//    val company = "Dicoding"
+//    println(company.reversed())
+//    println(company.toUpperCase())
+//    println(company.toLowerCase())
+//}
+
 fun main() {
-    val company = "Dicoding"
-    println(company.reversed())
-    println(company.toUpperCase())
-    println(company.toLowerCase())
+    val someString = "Dicoding"
+
+    println(someString.reversed())
+    println(someString.toUpperCase())
+    println(someString.toLowerCase())
 }
