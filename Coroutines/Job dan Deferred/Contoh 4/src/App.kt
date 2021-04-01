a5e4b954b68630d66eaaeba9a2ad031847be572e
TriCodeIT <bayu165kusuma@gmail.com>
2021-04-01 22:18:38 +0700

    Job

--- Coroutines/Job dan Deferred/Contoh 4/src/App.kt
+++ Coroutines/Job dan Deferred/Contoh 4/src/App.kt
@@ -13,3 +13,20 @@ fun main() = runBlocking {
         println("Job is cancelled")
     }
 }
+
+//@InternalCoroutinesApi
+//fun main() = runBlocking {
+//    val job = launch {
+//        delay(5000)
+//        println("Start new job!")
+//    }
+//
+//    delay(2000)
+//    job.cancel(cause = CancellationException("Time is up!"))
+//    println("Cancelling job...")
+//    if (job.isCancelled){
+//        println("Job is cancelled because ${job.getCancellationException().message}")
+//    }
+//}
+
+
